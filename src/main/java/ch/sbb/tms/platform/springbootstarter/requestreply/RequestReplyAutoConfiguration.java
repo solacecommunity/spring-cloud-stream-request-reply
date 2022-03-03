1f772faf97b4666c21a731db53fc9034eda926ad
René Schäfer <rene.schaefer2@sbb.ch>
2022-03-03 16:39:54 +0100

    cleanup

--- src/main/java/ch/sbb/tms/platform/springbootstarter/requestreply/RequestReplyAutoConfiguration.java
+++ src/main/java/ch/sbb/tms/platform/springbootstarter/requestreply/RequestReplyAutoConfiguration.java
@@ -5,19 +5,16 @@
 package ch.sbb.tms.platform.springbootstarter.requestreply;
 
 import java.util.function.Consumer;
-import java.util.function.Supplier;
 
-import ch.sbb.tms.platform.springbootstarter.requestreply.config.RequestReplyProperties;
-import ch.sbb.tms.platform.springbootstarter.requestreply.service.RequestReplyService;
-import ch.sbb.tms.platform.springbootstarter.requestreply.service.header.parser.SolaceHeaderParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.cloud.function.context.FunctionRegistration;
 import org.springframework.cloud.function.context.FunctionType;
@@ -36,16 +33,20 @@ import org.springframework.core.annotation.Order;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 
+import ch.sbb.tms.platform.springbootstarter.requestreply.config.RequestReplyProperties;
+import ch.sbb.tms.platform.springbootstarter.requestreply.service.RequestReplyService;
+import ch.sbb.tms.platform.springbootstarter.requestreply.service.header.parser.SolaceHeaderParser;
+
 @Configuration
 @ConditionalOnClass({MessageChannel.class})
 @ComponentScan("ch.sbb.tms.platform.springbootstarter.requestreply.service")
 @AutoConfigureAfter({
-        ContextFunctionCatalogAutoConfiguration.class,
-        PropertySourcesPlaceholderConfigurer.class,
-        BinderFactoryAutoConfiguration.class
+    ContextFunctionCatalogAutoConfiguration.class,
+    PropertySourcesPlaceholderConfigurer.class,
+    BinderFactoryAutoConfiguration.class
 })
 @AutoConfigureBefore({
-        FunctionConfiguration.class
+    FunctionConfiguration.class
 })
 @Order(Ordered.LOWEST_PRECEDENCE)
 @EnableConfigurationProperties(RequestReplyProperties.class)
@@ -53,6 +54,9 @@ public class RequestReplyAutoConfiguration implements ApplicationContextInitiali
     private static final int SOLACE_CONFIGURERS_PRIORITY = 200;
 
     private static final Logger LOG = LoggerFactory.getLogger(RequestReplyAutoConfiguration.class);
+
+    @Autowired
+    private RequestReplyProperties requestReplyProperties;
 
     @Bean
     @Order(SOLACE_CONFIGURERS_PRIORITY)
@@ -72,28 +76,33 @@ public class RequestReplyAutoConfiguration implements ApplicationContextInitiali
     }
 
     @Override
-    public void initialize(GenericApplicationContext context) {
-        RequestReplyProperties requestReplyProperties = Binder.get(context.getEnvironment())
-                .bind("spring.cloud.stream.requestreply", RequestReplyProperties.class)
-                .get();
+    public void initialize(final GenericApplicationContext context) {
+        final BindResult<RequestReplyProperties> bindResult = Binder.get(context.getEnvironment()).bind("spring.cloud.stream.requestreply",
+                RequestReplyProperties.class);
 
-        for (String bindingName : requestReplyProperties.getBindingMappingNames()) {
+        if (!bindResult.isBound()) {
+            return;
+        }
+
+        final RequestReplyProperties requestReplyProperties = bindResult.get();
+
+        for (final String bindingName : requestReplyProperties.getBindingMappingNames()) {
             context.registerBean(
                     bindingName,
                     FunctionRegistration.class,
                     () -> new FunctionRegistration<Consumer<Message<?>>>(msg -> {
                         ((RequestReplyService) context.getBean("requestReplyService")).onReplyReceived(msg);
                     })
-                            .type(new FunctionType(
+                    .type(new FunctionType(
+                            ResolvableType.forClassWithGenerics(
+                                    Consumer.class,
                                     ResolvableType.forClassWithGenerics(
-                                            Consumer.class,
-                                            ResolvableType.forClassWithGenerics(
-                                                    Message.class,
-                                                    Object.class
+                                            Message.class,
+                                            Object.class
                                             )
                                     ).getType()
                             ))
-            );
+                    );
             LOG.info("Register binding: " + bindingName + " for receiving replies");
         }
     }
